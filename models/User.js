b1c000f30646245ccfeb4c25379e711498543402
Moaz Ali <151751659+40moaz@users.noreply.github.com>
2024-06-25 22:28:00 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require( 'mongoose' );
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema( {
     fullName: { type: String, required: true, unique: true },
     email: { type: String, required: true },
     phone: { type: String, required: true },
-    date_of_birth: { type: Date, required: true},
+    date_of_birth: { type: String, required: true },
     profileImage: { type: String, required: false },
     password: { type: String, required: true }
-});
+} );
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model( 'User', UserSchema );
 
 module.exports = User;
